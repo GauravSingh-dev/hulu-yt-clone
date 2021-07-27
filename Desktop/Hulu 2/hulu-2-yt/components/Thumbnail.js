0c822c94bd0a7e0061fa57e514ed4c2f951fed77
Gaurav Singh <gauravkutlehria3@gmail.com>
2021-07-27 17:49:15 +0530

    initial commit

--- Desktop/Hulu 2/hulu-2-yt/components/Thumbnail.js
+++ Desktop/Hulu 2/hulu-2-yt/components/Thumbnail.js
@@ -1,10 +1,12 @@
 import Image from "next/image"; 
 import {ThumbUpIcon} from "@heroicons/react/outline";
 import {forwardRef} from "react";
-const Thumbnail= forwardRef(({result}, ref) => {
+const Thumbnail= forwardRef (({result}, ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
     return (
-        <div ref={ref} className=" p-2 group cursor-pointer transition duration-200 
+        <div 
+        ref={ref} 
+        className=" p-2 group cursor-pointer transition duration-200 
         ease-in transform sm:hover:scale-105 hover:z-50">
            < Image 
            layout="responsive" 
@@ -19,9 +21,11 @@ const Thumbnail= forwardRef(({result}, ref) => {
 
            <div className="p-2">
                <p className=" truncate max-w-md">{result.overview}</p>
+
                <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold'>
                    {result.title || result.original_name}
                </h2 >
+
                <p className="flex items-center opacity-0 group-hover:opacity-100"> 
                    {result.media_type && `${result.media_type} `}{" "}
                       {result.release_date || result.first_air_date}{""}
@@ -32,5 +36,7 @@ const Thumbnail= forwardRef(({result}, ref) => {
         </div>
     );
 } )
+
+Thumbnail.displayName = 'Thumbnail';
 
 export default Thumbnail;
